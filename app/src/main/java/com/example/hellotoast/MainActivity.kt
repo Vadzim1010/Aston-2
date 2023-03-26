4ecfd40061a72e11b811b9d3c8d08eb094468168
Vadim Evmushkov <1010muad.dib@gmail.com>
2023-03-26 23:22:45 +0300

    done 2_3

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -1,58 +1,30 @@
 package com.example.hellotoast
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.hellotoast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var countButton: Button? = null
-    private var countTextView: TextView? = null
-    private var editText: EditText? = null
-    private var count: Int = 0
+    private lateinit var binding: ActivityMainBinding
+
+    private val photoLauncher = registerForActivityResult(
+        ActivityResultContracts.TakePicturePreview()
+    ) { bitmap ->
+        binding.photo.setImageBitmap(bitmap)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        initView()
-        setValue(savedInstanceState)
-        count()
-    }
-
-    private fun initView() {
-        countButton = findViewById(R.id.count_button)
-        countTextView = findViewById(R.id.counter_text_view)
-        editText = findViewById(R.id.edit_text)
-    }
-
-    private fun setValue(savedInstanceState: Bundle?) {
-        val currentCount = savedInstanceState?.getInt(KEY_INT)
-        val currentString = savedInstanceState?.getString(KEY_STRING)
-
-        countTextView?.text = currentCount?.toString() ?: count.toString()
-        count = currentCount ?: 0
-
-        editText?.setText(currentString)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(KEY_INT, count)
-        outState.putString(KEY_STRING, editText?.text.toString())
-    }
-
-    private fun count() {
-        countButton?.setOnClickListener {
-            count += 1
-            countTextView?.text = count.toString()
+        with(binding) {
+            takePhotoButton.setOnClickListener {
+                photoLauncher.launch(null)
+            }
         }
-    }
-
-    companion object {
-        private const val KEY_INT = "key_int"
-        private const val KEY_STRING = "key_string"
     }
 }
